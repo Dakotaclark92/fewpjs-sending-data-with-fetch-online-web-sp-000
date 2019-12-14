4079f122d347f2b70b31cd887eb935fe22fa183a
Dakota Clark <dakotaclark92@yahoo.com>
2019-12-14 00:13:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {
+    method: 'POST',
+    body: {
+      firstName: "Jason",
+      registryMessage: "Hello, this is my message to the registry"
+    }
+  })
+  .then(function(result) => {
+    return result.json();
+  }).then(function(json) {
+    return json.message;
+  });
+}
