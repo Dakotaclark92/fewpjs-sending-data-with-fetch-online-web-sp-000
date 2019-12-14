0122d1f5c9b2e42870bb60aa95ed217f5e6a9411
Dakota Clark <dakotaclark92@yahoo.com>
2019-12-14 00:14:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function registerSelf() {
     return json.message;
   });
 }
+
+function errorSelf() {
+return fetch("http://guestbook.example.com/register-error", {
+    "method": "POST",
+    "body": {
+      "firstName": "Byron the Poodle",
+      "registryMessage": "Bite!"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  }).catch(function(x) {
+    return ":(";
+  });
+}
